98f728fdff88cc3cd4581dab23edea998c423613
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-04-01 18:33:56 -0300

    consertando a 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -10,7 +10,7 @@ function loginReducer(state = INITIAL_STATE, action) {
   case LOGIN_INPUT_EMAIL:
     return {
       ...state,
-      email: action.email,
+      email: action.value,
     };
   default:
     return state;
